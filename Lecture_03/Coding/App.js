1b99bef8df62c3ee3a612efe314e6575c7f8315d
abhay0106 <abhaykochhar0106@gmail.com>
2023-01-09 01:09:30 +0530

    Lecture_03 Coding Assignment

--- Lecture_03/Coding/App.js
+++ Lecture_03/Coding/App.js
@@ -1,21 +1,58 @@
 import React from "react";
-import ReactDOM  from "react-dom/client";
+import ReactDOM from "react-dom/client";
+
+// Create a Nested header Element using React.createElement (h1,h2,h3,inside div with class title)?
+
+const heading1 = React.createElement(
+    'h1',
+    {
+      id: 'heading1',
+      key: 'h1',
+    },
+    'Heading 1'
+  );
+  
+  const heading2 = React.createElement(
+    'h2',
+    {
+      id: 'heading2',
+      key: 'h2',
+    },
+    'Heading 2'
+  );
+  
+  const heading3 = React.createElement(
+    'h3',
+    {
+      id: 'heading3',
+      key: 'h3',
+    },
+    'Heading 3'
+  );
+  
+  const header = React.createElement(
+    'div',
+    {
+      className: 'title',
+    },
+    [heading1, heading2, heading3]
+  );
 
 //Functional Component
 const Title = () => (
-    <h1 id = "title" key="h1">
+    <h1 id="title" key="h1">
         Namaste React
     </h1>
 );
 
 //React Element
-const Title2 =(
+const Title2 = (
     <h1>This is a React Element</h1>
 )
 
 //Functional Component
-const HeaderComponent = () => {
-    return(
+const Header = () => {
+    return (
         <div>
             {/* We can also call the functional component as below */}
             {/* {Title()} */}
@@ -23,10 +60,10 @@ const HeaderComponent = () => {
             {/* Calling the React Element */}
             {Title2}
 
-            
+
             {/*  Composition of Component(Adding Component inside another component) */}
-            {<Title/>}
-            
+            {<Title />}
+
             <h1>Namaste React a functional Component</h1>
             <h2>This is a h2 tag</h2>
 
@@ -34,6 +71,37 @@ const HeaderComponent = () => {
     );
 };
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+// const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeaderComponent/>);
+// root.render(<Header/>);
+
+
+// Create Header Component from Scratch using Functional Comp.
+
+
+const SearchBar = () => {
+    return (
+      <>
+        <input placeholder="Search here" className="search-bar" />
+      </>
+    );
+  };
+  
+  const HeaderComponent = () => {
+    return (
+      <div className="container">
+        <img
+          src="https://www.vectorlogo.zone/logos/reactjs/reactjs-ar21.png"
+          alt=""
+        />
+        <SearchBar />
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png"
+          alt=""
+        />
+      </div>
+    );
+  };
+  
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(<HeaderComponent />);
