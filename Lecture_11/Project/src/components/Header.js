3391acaf9a1ed3572f13ecdfa4a12d8822a93e3c
abhay0106 <abhaykochhar0106@gmail.com>
2023-02-05 22:22:32 +0530

    Lecture 11 Props Drilling, Implement accordion and React Context

---
+++ Lecture_11/Project/src/components/Header.js
@@ -0,0 +1,59 @@
+import {  useState,useContext } from 'react';
+import {Link} from "react-router-dom";
+import useOnline from "../utils/useOnline"
+import UserContext from '../utils/UserContext';
+
+const loggedInUser = () => {
+  // API call to check authentication
+  return false;
+};
+
+const Title = () => (
+  <a href="/">
+    <img
+      className="h-28 p-2"
+      alt="logo"
+      src="https://yt3.ggpht.com/ytc/AMLnZu_EC-ECXAxRAixWGEfMsE1rdSoetBHyxmLNdtCB=s900-c-k-c0x00ffffff-no-rj"
+    />
+  </a>
+);
+
+export const Header = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const isOnline = useOnline();
+
+  const {user} = useContext(UserContext);
+
+  return (
+    <div className="flex justify-between bg-pink-50 shadow-lg sm:bg-blue-50 md:bg-yellow-500">
+      <Title />
+      <div className="nav-items">
+        <ul className="flex py-10">
+          <li className="px-2">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="px-2">
+            <Link to="/about">About</Link>
+          </li>
+
+          <li className="px-2">
+            <Link to = "/contact">Contact</Link>
+            </li>
+          <li>Cart</li>
+          <li className="px-2">
+            <Link to = "/instamart">Instamart</Link> 
+          </li>
+        </ul>
+      </div>
+      <span className="p-10 text-bold text-red-800">{user.name}</span>
+      {isLoggedIn ? (
+        <button onClick={() => setIsLoggedIn(false)}>Logout</button>
+      ) : (
+        <button onClick={() => setIsLoggedIn(true)}>Login</button>
+      )}
+    </div>
+  );
+};
+
+export default Header;
